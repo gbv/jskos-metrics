78f747f17f7a9fa99063ee7b20e08617cd332383
Jakob Voss <voss@gbv.de>
2019-05-27 10:41:42 +0200

    scheme: check number of distinct URIs

--- scheme/conceptNumber.sh
+++ scheme/conceptNumber.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-cat "$1" | wc -l
+NUMBER=`cat "$1" | wc -l`
+UNIQ=`jq .uri "$1" | sort | uniq | wc -l`
+
+if [ "$NUMBER" -ne "$UNIQ" ]; then
+  echo "Found $NUMBER concepts but $UNIQ distinct URIs!" >&2
+fi
+
+echo $NUMBER
